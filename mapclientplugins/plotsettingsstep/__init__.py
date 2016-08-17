61f50e3b6f7b5621e6f26ad730d203d76d5008e9
Hugh Sorby <h.sorby@auckland.ac.nz>
2016-08-18 10:37:59 +1200

    Set location of source.

--- mapclientplugins/plotsettingsstep/__init__.py
+++ mapclientplugins/plotsettingsstep/__init__.py
@@ -6,7 +6,7 @@ MAP Client Plugin
 __version__ = '0.1.0'
 __author__ = 'Hugh Sorby'
 __stepname__ = 'Plot Settings'
-__location__ = ''
+__location__ = 'https://github.com/mapclient-plugins/plotsettings/archive/v0.1.0.zip'
 
 # import class that derives itself from the step mountpoint.
 from mapclientplugins.plotsettingsstep import step
